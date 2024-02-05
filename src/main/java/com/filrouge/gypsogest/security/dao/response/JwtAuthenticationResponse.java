f4aad9e2ba99e4721de32ef3fed94c6b6561d13a
md_naciri <med.ar.naciri@gmail.com>
2024-02-05 16:53:41 +0100

    implement CorsConfig

--- src/main/java/com/filrouge/gypsogest/security/dao/response/JwtAuthenticationResponse.java
+++ src/main/java/com/filrouge/gypsogest/security/dao/response/JwtAuthenticationResponse.java
@@ -1,4 +1,12 @@
 package com.filrouge.gypsogest.security.dao.response;
 
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtAuthenticationResponse {
+    private String token;
 }
