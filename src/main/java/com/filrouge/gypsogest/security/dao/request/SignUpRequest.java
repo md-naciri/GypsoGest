4ad86cdaecfe68db781f1be9eb10c98be6ad8395
md_naciri <med.ar.naciri@gmail.com>
2024-02-05 16:52:11 +0100

    implement SignUpRequest

--- src/main/java/com/filrouge/gypsogest/security/dao/request/SignUpRequest.java
+++ src/main/java/com/filrouge/gypsogest/security/dao/request/SignUpRequest.java
@@ -1,4 +1,14 @@
 package com.filrouge.gypsogest.security.dao.request;
 
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class SignUpRequest {
+    private String name;
+    private String username;
+    private String password;
 }
