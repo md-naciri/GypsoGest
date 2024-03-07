adcb17f16cca427d510e163a0cf1433716d093b1
md_naciri <med.ar.naciri@gmail.com>
2024-03-07 22:41:49 +0100

    add validateToken method in AuthenticationController

--- src/main/java/com/filrouge/gypsogest/web/auth/AuthenticationController.java
+++ src/main/java/com/filrouge/gypsogest/web/auth/AuthenticationController.java
@@ -4,8 +4,12 @@ import com.filrouge.gypsogest.security.dao.request.SignInRequest;
 import com.filrouge.gypsogest.security.dao.request.SignUpRequest;
 import com.filrouge.gypsogest.security.dao.response.JwtAuthenticationResponse;
 import com.filrouge.gypsogest.service.AuthenticationService;
+import com.filrouge.gypsogest.service.JwtService;
+import com.filrouge.gypsogest.service.UserService;
+import com.filrouge.gypsogest.web.vm.TokenValidationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final UserService userService;
+    private final JwtService jwtService;
     @PostMapping("/signup")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest signUpRequest){
         return ResponseEntity.ok(authenticationService.signUp(signUpRequest));
@@ -24,5 +30,23 @@ public class AuthenticationController {
     @PostMapping("/signing")
     public ResponseEntity<JwtAuthenticationResponse> signing(@RequestBody SignInRequest signInRequest){
         return ResponseEntity.ok(authenticationService.signIn(signInRequest));
+    }
+    @PostMapping("/validate-token")
+    public ResponseEntity<Boolean> validateToken(@RequestBody TokenValidationRequest validationRequest) {
+        String token = validationRequest.getToken();
+
+        try {
+            UserDetails userDetails = userService.userDetailsService()
+                    .loadUserByUsername(jwtService.extractUserName(token));
+
+            if (jwtService.isTokenValid(token, userDetails)) {
+                return ResponseEntity.ok(true);
+            } else {
+                return ResponseEntity.ok(false);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return ResponseEntity.ok(false);
+        }
     }
 }
